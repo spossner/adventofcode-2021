dd612f7ce7877c17c3b02d51e7c26983d9182410
spossner <github@possner.de>
2021-12-04 08:30:27 +0100

    bingo finished

--- 04.py
+++ 04.py
@@ -11,10 +11,12 @@ class Solution:
         numbers = [int(a) for a in data[0].split(',')]
         cards = []
         start = 2
+        winners = []
         while start < len(data):
-            card = [[0] * 5 for i in range(5)]
+            card = [[0] * 6 for i in range(6)] # extra row and extra column
             for y in range(5):
                 card[y] = [int(a) for a in data[start+y].split()]
+                card[y].append(0)
             cards.append(card)
             start = start + 6 # skip empty line
 
@@ -22,11 +24,23 @@ class Solution:
         print(cards)
 
         for n in numbers:
-            for card in cards:
-                for row in card:
-                    i = row.index(n)
+            for id, card in enumerate(cards):
+                if id in winners:
+                    continue
+                for row in card[:-1]:
+                    try:
+                        i = row[:-1].index(n)
+                        row[i] = (n, True) # mark cell and make it not found by subsequent searches
+                        row[-1] += 1
+                        card[-1][i] += 1
+                        if row[-1] == 5 or card[-1][i] == 5:
+                            winners.append(id)
+                            if not modified or len(winners) == len(cards):
+                                return (n, [row[:-1] for row in card[:-1]]) # found winner
+                    except ValueError:
+                        pass
 
-
+        return None
 
 
 if __name__ == '__main__':
@@ -35,8 +49,13 @@ if __name__ == '__main__':
         script = script.split('-')[0]
     s = Solution()
 
-    with open(f'{script}-dev.txt') as f:
-        result = s.solve(f.read().strip().splitlines())
-        print(result)
+    with open(f'{script}.txt') as f:
+        n, card = s.solve(f.read().strip().splitlines(), True)
+        sum = 0
+        print(n, card)
+        for row in card:
+            for v in row:
+                sum = sum + (v if type(v) == int else 0)
+        print(sum*n)
 
 
