cb7279023d97b0cc1f0e9296edf82466336a44cb
spossner <github@possner.de>
2021-12-09 09:28:08 +0100

    comments

--- 08.py
+++ 08.py
@@ -3,32 +3,23 @@ import sys
 from collections import defaultdict
 
 SEGMENTS = {
-    1: 'cf',
-    7: 'acf',
-    4: 'bcdf',
+    # special in 0-2
+    'cf': '1',
+    'acf': '7',
+    'bcdf': '4',
 
-    2: 'acdeg',
-    3: 'acdfg',
-    5: 'abdfg',
+    # 5 segments in 3-5
+    'acdeg': '2',
+    'acdfg': '3',
+    'abdfg': '5',
 
-    0: 'abcefg',
-    6: 'abdefg',
-    9: 'abcdfg',
+    # 6 segments in 6-8
+    'abcefg': '0',
+    'abdefg': '6',
+    'abcdfg': '9',
 
-    8: 'abcdefg',
-}
-
-cipher = {
-    "cf": 1,
-    "acf": 7,
-    "bcdf": 4,
-    "acdeg": 2,
-    "acdfg": 3,
-    "abdfg": 5,
-    "abcefg": 0,
-    "abdefg": 6,
-    "abcdfg": 9,
-    "abcdefg": 8,
+    # all 7 segments in 9
+    'abcdefg': '8',
 }
 
 class Solution:
@@ -45,58 +36,56 @@ class Solution:
 
         return sum((counts[2],counts[4],counts[3],counts[7]))
 
-    def set_candidate(self, candidates, mappings, candidate_set):
-        for no in mappings:
-            candidates[no] = candidates[no].intersection(candidate_set)
-        for no in set('abcdefg') - set(mappings):
-            candidates[no] = candidates[no] - candidate_set
-
-    def solve3(self, data, modified=False):
+    def solve2(self, data, modified=False):
         if type(data) is not list:
             data = [data]
 
         sum_all = 0
 
         for line in data:
-            patterns, digits = line.strip().split("|")
-            right = digits
-            patterns = sorted((set(dig) for dig in patterns.strip().split(" ")), key=len)
-            digits = [set(dig) for dig in digits.strip().split(" ")]
+            candidates, right = line.strip().split("|")
+            candidates = sorted((set(n) for n in candidates.strip().split(" ")), key=len)
+            digits = [set(n) for n in right.strip().split(" ")]
 
-            set_cf, set_acf, set_bcdf = patterns[:3]
-            set_a = set_acf - set_cf
-            set_bd = set_bcdf - set_acf
+            candidates_cf, candidates_acf, candidates_bcdf = candidates[:3] # all samples contain the three numbers
+            assert len(candidates_cf) == 2
+            assert len(candidates_acf) == 3
+            assert len(candidates_bcdf) == 4
+
+            candidates_bd = candidates_bcdf - candidates_acf # 4 without 7 gives b and d candidates
+            candidates_a = candidates_acf - candidates_cf # 7 without the 1..
             for i in range(3, 9):
-                patterns[i] -= set_a
-            set_g = set.intersection(*patterns[3:9])
+                candidates[i] = candidates[i] - candidates_a
+
+            # all 5/6 had a and g together -> w/a juts g left
+            candidates_g = set.intersection(*candidates[3:9])
             for i in range(3, 9):
-                patterns[i] -= set_g
-            set_d = set.intersection(*patterns[3:6])
-            set_b = set_bd - set_d
-            set_cef = set.union(*patterns[3:6]) - set_b - set_d
-            set_e = set_cef - set_bcdf
+                candidates[i] = candidates[i] - candidates_g
+
+            # d left in 5 segments digits
+            candidates_d = set.intersection(*candidates[3:6])
+            candidates_b = candidates_bd - candidates_d
+            # all the rest from the 5 segments w/o b and d
+            candidates_cef = set.union(*candidates[3:6]) - candidates_b - candidates_d
+            candidates_e = candidates_cef - candidates_bcdf
             for i in range(6, 9):
-                patterns[i] = patterns[i] - set_b - set_d - set_e
-            set_f = set.intersection(*patterns[6:9])
-            set_c = set_cf - set_f
+                candidates[i] = candidates[i] - candidates_b - candidates_d - candidates_e
+            candidates_f = set.intersection(*candidates[6:9])
+            candidates_c = candidates_cf - candidates_f
 
-            transm = {
-                tuple(*set_a)[0]: "a",
-                tuple(*set_b)[0]: "b",
-                tuple(*set_c)[0]: "c",
-                tuple(*set_d)[0]: "d",
-                tuple(*set_e)[0]: "e",
-                tuple(*set_f)[0]: "f",
-                tuple(*set_g)[0]: "g",
+            mapping = {
+                list(candidates_a)[0]: "a",
+                list(candidates_b)[0]: "b",
+                list(candidates_c)[0]: "c",
+                list(candidates_d)[0]: "d",
+                list(candidates_e)[0]: "e",
+                list(candidates_f)[0]: "f",
+                list(candidates_g)[0]: "g",
             }
 
-            num = int(
-                "".join(
-                    str(cipher["".join(sorted(transm[d] for d in ds))]) for ds in digits
-                )
-            )
-            print(f"{right}: {num}")
-            sum_all += num
+            result = [SEGMENTS["".join(sorted(mapping[d] for d in digit))] for digit in digits]
+            print(f"{right}: {result}")
+            sum_all += int("".join(result))
 
         return sum_all
 
@@ -118,12 +107,12 @@ if __name__ == '__main__':
 
     # print(s.solve3('acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'))
 
-    # with open(f'{script}-dev.txt') as f:
-    #    result = s.solve3(f.read().strip().splitlines(), True)
-    #    print(result)
+    with open(f'{script}-dev.txt') as f:
+       result = s.solve2(f.read().strip().splitlines(), True)
+       print(result)
     #
     with open(f'{script}.txt') as f:
-        result = s.solve3(f.read().strip().splitlines(), True)
+        result = s.solve2(f.read().strip().splitlines(), True)
         print(result)
 
 
