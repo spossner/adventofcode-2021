1ff6764bca445d2250df458a99fd23230e818c32
spossner <github@possner.de>
2021-12-11 10:14:01 +0100

    some colored visulization

--- 09.py
+++ 09.py
@@ -2,6 +2,19 @@ import os
 import sys
 from collections import defaultdict, deque
 
+COLORS = {
+    0: u"\u001b[38;5;0m",
+    1: u"\u001b[38;5;17m",
+    2: u"\u001b[38;5;19m",
+    3: u"\u001b[38;5;21m",
+    4: u"\u001b[38;5;26m",
+    5: u"\u001b[38;5;30m",
+    6: u"\u001b[38;5;38m",
+    7: u"\u001b[38;5;44m",
+    8: u"\u001b[38;5;50m",
+    9: u"\u001b[38;5;15m",
+}
+
 
 class Solution:
     def solve(self, data, modified=False):
@@ -15,10 +28,15 @@ class Solution:
         w = len(grid[0])
         d = []
         for y in range(h):
+
             for x in range(w):
                 v = grid[y][x]
+                if v in COLORS:
+                    print(COLORS[v],end='')
+                print(v, end='')
                 if (y == 0 or v < grid[y - 1][x]) and (y == h-1 or v < grid[y + 1][x]) and (x == 0 or v < grid[y][x - 1]) and (x == w-1 or v < grid[y][x + 1]):
                     d.append((x, y))
+            print()
         basins = []
         visited = set()
         for p in d:
@@ -49,7 +67,7 @@ class Solution:
                     b.append(np)
                     d.append(np)
                     visited.add(np)
-        print(f"found {b} with size {size}")
+        #print(f"found {b} with size {size}")
         return size
 
 
